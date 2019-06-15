0eda89ec0a412a2b3f1acd2f8e7acfe92ec3993c
mckenney5 <mckenneyadam@gmail.com>
2019-06-14 23:16:45 -0400

    Added an init check, upped version number
    Also made errors wait for input before going away

--- src/engine.h
+++ src/engine.h
@@ -16,7 +16,7 @@ copies or substantial portions of the Software.
 
 #ifndef _USING_ENGINE
 #define _USING_ENGINE
-#define _ENGINE_VERSION "Alpha 0.0.01"
+#define _ENGINE_VERSION "Alpha 0.0.02"
 #define _ENGINE_DEBUG 0		/* Set to 1 for debugging, 0 for none */
 #define ENGINE_NAME "Ncurses Engine"
 
@@ -71,6 +71,9 @@ void SetWindowColor(const unsigned short, const unsigned short);
 /* Sets the default title color */
 void SetTitleColor(const unsigned short, const unsigned short);
 
+/* Creates a yes/no dialog box */
+//void Dialog(char*, char*);
+
 
 /* -- */
 
@@ -81,9 +84,17 @@ void SetTitleColor(const unsigned short, const unsigned short);
 static unsigned int Max_y, Max_x;		/* User defined screen size */
 static unsigned int Real_Max_y, Real_Max_x;	/* Actual screen size */
 static unsigned short UseWarn = 1;		/* When set to 1, Warnings will apear as they are called */
+static unsigned short Initialized = 0;	/* Used to track if Init has been ran, only eddited by Init and CleanUp */
 
 void Init(){
 /* This is all needed for ncurses.h */
+
+	/* Checks if Init has been called before */
+	if(Initialized == 1){
+		Warn("In Init, ncurses has already been initialized. Ignoring");
+		return;
+	}
+	Initialized = 1;
 
 	/* Create the screen */
 	initscr();
@@ -143,9 +154,11 @@ void SetMaxYX(const unsigned int y, const unsigned int x){
 	Max_x = x;
 }
 
-
 void GenBox(const unsigned int Width, const unsigned int Height, const short UseShadow){
 /* Displays a box */
+	/* Checks if Init has been called before */
+	if(Initialized == 0) Warn("In GenBox, ncurses has not been initialized. Call Init() before use");
+
 	if(Real_Max_y < Height)
 		Warn("In GenBox, Height is set higher than screen size");
 	if(Real_Max_x < Width)
@@ -175,8 +188,12 @@ void GenBox(const unsigned int Width, const unsigned int Height, const short Use
 		attron(COLOR_PAIR(1));
 	}
 }
+
 void PutTitle(const unsigned int Width, const unsigned int Height, const char* Msg, const unsigned int Size){
 /* Displays a title on the top line of the box */
+	/* Checks if Init has been called before */
+	if(Initialized == 0) Warn("In PutTitle, ncurses has not been initialized. Call Init() before use");
+
 	int i;
 	attron(COLOR_PAIR(2));
 	unsigned int Mid_x = Max_x/2;
@@ -193,68 +210,93 @@ void PutTitle(const unsigned int Width, const unsigned int Height, const char* M
 	
 	attron(COLOR_PAIR(1));
 }
+
 void Pause(){
 /* TODO make better pause functions */
 /* Simple wait functions that waits for a user to press a key */
+	/* Checks if Init has been called before */
+	if(Initialized == 0) Warn("In Pause, ncurses has not been initialized. Call Init() before use");
+
 	printw(" Press any key to continue...");
 	getch();
 }
+
 static void Warn(char * Mesg){
 /* Display a warning if it is enabled */
 	if(UseWarn == 0) return;
-	move(0,0);
-	const char Prefix[] = "(W) ";
+	const char Prefix[] = "[Warning] ";
 	fprintf(stderr, "%s: %s%s\n", ENGINE_NAME, Prefix, Mesg);
+	/* Checks if Init has been called before */
+	if(Initialized == 0) return;
+	move(0,0);
 	attron(COLOR_PAIR(3));
 	printw("%s: %s%s\n", ENGINE_NAME, Prefix, Mesg);
 	attron(COLOR_PAIR(1));
+	if(_ENGINE_DEBUG) getch();
 }
+
 static void Error(char * Mesg){
 /* Display an error */
-	move(0,0);
-	const char Prefix[] = "(E) ";
+	const char Prefix[] = "[Error] ";
 	fprintf(stderr, "%s: %s%s\n", ENGINE_NAME, Prefix, Mesg);
+	if(Initialized == 0) return;
+	move(0,0);
 	attron(COLOR_PAIR(4));
 	printw("%s: %s%s\n", ENGINE_NAME, Prefix, Mesg);
 	attron(COLOR_PAIR(1));
+	getch();
 }
+
 static void CleanUp(){
 /* Destroys window before exit */
 	endwin();
+	Initialized = 0; //since ncurses is closing, allow it to be restarted
 }
+
 void DisableWarnings(){
-	UseWarn = 0;	
+	UseWarn = 0;
 }
+
 void EnableWarnings(){
 	UseWarn = 1;
 }
+
 void Minimize(){
 /* Suspend ncurses */
+	if(Initialized == 0) Warn("In Minimize, ncurses has not been initialized. Call Init() before use");
 	def_prog_mode();
 	endwin();
 }
+
 void Maximize(){
 /* Resume ncurses */
+	if(Initialized == 0) Warn("In Maximize, ncurses has not been initialized. Call Init() before use");
 	reset_prog_mode();
 	refresh();
 }
+
 void SetColor(const unsigned short FColor, const unsigned short BColor){
 /* Checks if it is given a valid color, then sets it */
+	if(Initialized == 0) Warn("In SetColor, ncurses has not been initialized. Call Init() before use");
 	if(FColor > 7 || BColor > 7){
 		Error("In SetColor, Color does not exist, colors are 0 - 7");
 	} else {
 		init_pair(1, FColor, BColor);
 	}
 }
+
 unsigned int GetTopY(const unsigned int Height){
 	unsigned int Mid_y = Max_y/2;
 	return Mid_y-(Height/2);
 }
+
 unsigned int GetTopX(const unsigned int Width){
 	unsigned int Mid_x = Max_x/2;
 	return Mid_x-(Width/2);
 }
+
 void SetWindowColor(const unsigned short FColor, const unsigned short BColor){
+	if(Initialized == 0) Warn("In SetWindowColor, ncurses has not been initialized. Call Init() before use");
 	if(FColor > 7 || BColor > 7){
 		Error("In SetWindowColor, Color does not exist, colors are 0 - 7");
 	} else {
@@ -270,13 +312,20 @@ void SetWindowColor(const unsigned short FColor, const unsigned short BColor){
 		attron(COLOR_PAIR(1));
 	}
 }
+
 void SetTitleColor(const unsigned short FColor, const unsigned short BColor){
 /* Checks if it is given a valid color, then sets it */
+	if(Initialized == 0) Warn("In SetTitleColor, ncurses has not been initialized. Call Init() before use");
 	if(FColor > 7 || BColor > 7){
 		Error("In SetTitleColor, Color does not exist, colors are 0 - 7");
 	} else {
 		init_pair(2, FColor, BColor);
 	}
 }
+
+/*
+void Dialog(char* Title, char* Prompt){
+	return;
+}*/
 #endif
 
